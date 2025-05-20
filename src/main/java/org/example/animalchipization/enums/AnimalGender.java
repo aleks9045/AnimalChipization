84c8dae47aa33fdbe5a8495be936e0547972e395
aleks9045 <aleksey9045@gmail.com>
2025-05-20 14:39:56 +0300

    Add comments to enums

--- src/main/java/org/example/animalchipization/enums/AnimalGender.java
+++ src/main/java/org/example/animalchipization/enums/AnimalGender.java
@@ -1,6 +1,12 @@
 package org.example.animalchipization.enums;
 
+import org.example.animalchipization.models.Animal;
+
 /**
+ * Represents possible gender values for animal.
+ *
+ * <p>Used in {@link Animal} entity as a gender attribute
+ *
  * @author Aleksey
  */
 public enum AnimalGender {
--- src/main/java/org/example/animalchipization/enums/AnimalLifeStatus.java
+++ src/main/java/org/example/animalchipization/enums/AnimalLifeStatus.java
@@ -1,6 +1,12 @@
 package org.example.animalchipization.enums;
 
+import org.example.animalchipization.models.Animal;
+
 /**
+ * Represents possible life status values for animal.
+ *
+ * <p>Used in {@link Animal} entity as a life status attribute
+ *
  * @author Aleksey
  */
 public enum AnimalLifeStatus {
