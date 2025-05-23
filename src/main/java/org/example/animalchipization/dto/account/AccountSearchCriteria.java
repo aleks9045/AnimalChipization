5368d3ee65219ecaac265e6708015cfb5157c254
aleks9045 <aleksey9045@gmail.com>
2025-05-23 13:13:15 +0300

    Complete comment to AccountSearchCriteria

--- src/main/java/org/example/animalchipization/dto/account/AccountSearchCriteria.java
+++ src/main/java/org/example/animalchipization/dto/account/AccountSearchCriteria.java
@@ -4,20 +4,14 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import org.example.animalchipization.entities.Account;
 
-import java.util.Optional;
 
 /**
- * Contains all fields that
+ * Contains all fields that participates in search process for {@link Account} entity
  *
  * @author Aleksey
  */
-//@Setter
-//@Getter
 public record AccountSearchCriteria(@NotNull @NotBlank @Size(max = 255) String firstName,
                                     @NotNull @NotBlank @Size(max = 255) String lastName,
                                     @NotNull @NotBlank @Size(max = 255) @Email String email) {
