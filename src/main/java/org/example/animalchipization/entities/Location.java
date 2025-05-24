c5b6bb2176c37ab5e0dd3e09e50031d78cc590f7
aleks9045 <aleksey9045@gmail.com>
2025-05-24 18:53:21 +0300

    Add unique cinstraint to pair of latitude and longitude

--- src/main/java/org/example/animalchipization/entities/Location.java
+++ src/main/java/org/example/animalchipization/entities/Location.java
@@ -16,22 +16,26 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode
 @ToString
-@Table(name = "location", indexes = {
+@Table(name = "location",
+        indexes = {
         @Index(name = "idx_location_id", columnList = "location_id")
-})
+        },
+        uniqueConstraints = {
+        @UniqueConstraint(name = "location_cords_unique", columnNames = {"latitude", "longitude"})
+        }
+    )
 @Entity
 public class Location {
     @Id
-    @SequenceGenerator(name = "location_id_seq",
-            sequenceName = "location_seq_id",
-            allocationSize = 10)
+    @SequenceGenerator(name = "location_id_seq", sequenceName = "location_seq_id", allocationSize = 10)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "location_seq_id")
     @Column(name = "location_id", nullable = false)
     private Long locationId;
 
-    @Column(name = "latitude",nullable = false, unique = true)
+    @Column(name = "latitude", nullable = false)
     private Double latitude;
 
-    @Column(name = "longitude",nullable = false, unique = true)
+    @Column(name = "longitude", nullable = false)
     private Double longitude;
+
 }
