9826f03f6b55173677d022839561f73d5316cd4a
aleks9045 <aleksey9045@gmail.com>
2025-05-27 11:47:13 +0300

    Solve n+1 problem in AnimalRepository

--- src/main/java/org/example/animalchipization/repository/AnimalRepository.java
+++ src/main/java/org/example/animalchipization/repository/AnimalRepository.java
@@ -1,9 +1,13 @@
 package org.example.animalchipization.repository;
 
+import io.micrometer.common.lang.NonNullApi;
 import org.example.animalchipization.entities.Animal;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Works with Account entity
@@ -15,4 +19,16 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long>, JpaSpecificationExecutor<Animal> {
 
+    @EntityGraph(
+            type = EntityGraph.EntityGraphType.FETCH,
+            attributePaths = {
+                    "chipperId.accountId",
+                    "chippingLocationId.locationId",
+                    "animalTypes.animalTypeId",
+                    "visitedLocations",
+                    "visitedLocations.location"
+
+            }
+    )
+    Optional<Animal> findById(Long animalId);
 }
