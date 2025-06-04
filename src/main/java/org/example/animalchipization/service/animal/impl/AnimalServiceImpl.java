65d5958c6d4f7cda7a4f2a2ed4c6c8e55958d660
aleks9045 <aleksey9045@gmail.com>
2025-06-04 15:20:04 +0300

    Добавил генератор полей для таблиц
    Изменил магические строки в сервисах на сгенерированные значения
    Из-за несвоевременных коммитов в сервисах много других, не касающихся
    этой темы изменений

--- src/main/java/org/example/animalchipization/service/animal/impl/AnimalServiceImpl.java
+++ src/main/java/org/example/animalchipization/service/animal/impl/AnimalServiceImpl.java
@@ -2,19 +2,14 @@ package org.example.animalchipization.service.animal.impl;
 
 import jakarta.persistence.criteria.JoinType;
 import org.example.animalchipization.dto.animal.*;
-import org.example.animalchipization.entities.Animal;
-import org.example.animalchipization.entities.AnimalType;
-import org.example.animalchipization.entities.Location;
-import org.example.animalchipization.entities.VisitedLocation;
-import org.example.animalchipization.enums.AnimalLifeStatus;
-import org.example.animalchipization.enums.errors.AnimalError;
-import org.example.animalchipization.exception.entities.AnimalException;
+import org.example.animalchipization.entities.*;
 import org.example.animalchipization.mappers.animal.AnimalMapper;
 import org.example.animalchipization.repository.*;
 import org.example.animalchipization.service.JpaSpecificationBuilder;
 import org.example.animalchipization.service.animal.AnimalService;
 import org.example.animalchipization.service.animal.AnimalValidator;
 import org.example.animalchipization.service.location.LocationValidator;
+import org.example.animalchipization.service.location.impl.LocationValidatorImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,8 +17,7 @@ import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+
 import java.util.List;
 import java.util.Set;
 
@@ -51,7 +45,7 @@ public class AnimalServiceImpl implements AnimalService {
     @Transactional
     public AnimalDtoOut getAnimal(Long animalId) {
 
-        Animal animal = animalValidator.validateAndGetAnimal(animalId);
+        Animal animal = animalValidator.validateAndGetById(animalId);
 
         return animalMapper.toDto(animal);
     }
@@ -62,17 +56,12 @@ public class AnimalServiceImpl implements AnimalService {
 
         animalValidator.checkChipperExistence(animalDtoIn.getChipperId());
 
-        Location chippingLocation = locationValidator.validateAndGetLocation(animalDtoIn.getChippingLocationId());
+        locationValidator.checkExistence(animalDtoIn.getChippingLocationId());
 
         Set<AnimalType> animalTypes = animalValidator.validateAndGetAnimalTypes(animalDtoIn.getAnimalTypes());
 
         Animal animal = animalMapper.toEntity(animalDtoIn);
         animal.setAnimalTypes(animalTypes);
-
-        VisitedLocation visitedLocation =
-                new VisitedLocation(null, animal, chippingLocation);
-
-        animal.setVisitedLocations(List.of(visitedLocation));
 
         animalRepository.save(animal);
 
@@ -83,14 +72,16 @@ public class AnimalServiceImpl implements AnimalService {
     @Transactional
     public AnimalDtoOut updateAnimal(Long animalId, AnimalDtoUpdate animalDtoUpdate) {
 
-        Animal existingAnimal = animalValidator.validateAndGetAnimal(animalId);
-
-        animalValidator.checkAnimalUpdate(existingAnimal, animalDtoUpdate);
+        Animal existingAnimal = animalValidator.validateAndGetById(animalId);
 
         animalValidator.checkChipperExistence(animalDtoUpdate.getChipperId());
         animalValidator.checkChippingLocationExistence(animalDtoUpdate.getChippingLocationId());
 
+        animalValidator.checkAnimalUpdate(existingAnimal, animalDtoUpdate);
+
         animalMapper.updateEntity(existingAnimal, animalDtoUpdate);
+
+        animalValidator.setDeathTimeIfDead(existingAnimal);
 
         animalRepository.save(existingAnimal);
 
@@ -101,12 +92,11 @@ public class AnimalServiceImpl implements AnimalService {
     @Override
     public void deleteAnimalById(Long animalId) {
 
-        animalValidator.checkAnimalExistence(animalId);
-        try {
-            animalRepository.deleteById(animalId);
-        } catch (Exception e) {
-            throw new AnimalException(AnimalError.ANIMAL_STILL_LINKED);
-        }
+        animalValidator.checkExistence(animalId);
+
+        animalValidator.checkVisitedLocationsEmpty(animalId);
+
+        animalRepository.deleteById(animalId);
     }
 
     @Override
@@ -115,31 +105,31 @@ public class AnimalServiceImpl implements AnimalService {
 
         Specification<Animal> spec = Specification.where(
                 JpaSpecificationBuilder.<Animal>dateTimeFrom(
-                        "chippingDateTime",
+                        Animal_.CHIPPING_DATE_TIME,
                         animalSearchCriteria.startDateTime())
         ).and(
                 JpaSpecificationBuilder.dateTimeTo(
-                        "chippingDateTime",
+                        Animal_.CHIPPING_DATE_TIME,
                         animalSearchCriteria.endDateTime())
         ).and(
                 JpaSpecificationBuilder.join(
-                        "chipperId",
-                        "accountId",
+                        Animal_.CHIPPER_ID,
+                        Account_.ACCOUNT_ID,
                         animalSearchCriteria.chipperId(),
                         JoinType.INNER)
         ).and(
                 JpaSpecificationBuilder.join(
-                        "chippingLocationId",
-                        "locationId",
+                        Animal_.CHIPPING_LOCATION_ID,
+                        Location_.LOCATION_ID,
                         animalSearchCriteria.chippingLocationId(),
                         JoinType.INNER)
         ).and(
                 JpaSpecificationBuilder.equal(
-                        "lifeStatus",
+                        Animal_.LIFE_STATUS,
                         animalSearchCriteria.lifeStatus())
         ).and(
                 JpaSpecificationBuilder.equal(
-                        "gender",
+                        Animal_.GENDER,
                         animalSearchCriteria.gender())
         );
         Page<Animal> animalPage = animalRepository.findAll(spec, pageable);
